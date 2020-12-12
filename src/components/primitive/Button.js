ec21a7309e229dc75d4461aa61afeb3ae7c8202f
Lujan Sanchez <mlulasan@gmail.com>
2020-12-12 12:42:45 +0100

    removed context

--- src/components/primitive/Button.js
+++ src/components/primitive/Button.js
@@ -1,14 +1,11 @@
 import React, { useContext } from "react";
-import ThemeContext from "../../contexts/ThemeContext";
 
 const Button = ({ children, forwardedRef, ...props }) => {
-  const { theme } = useContext(ThemeContext);
 
   return (
     <button
       ref={forwardedRef}
       tabindex="0"
-      className={`button ${theme}`}
       {...props}
     >
       {children}
