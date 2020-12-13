c0dc5464e5fd9d1adb72279dc380734ab5806436
Josefina del Rey <delreyjo@gmail.com>
2020-12-13 07:08:25 -0300

    exporto register

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
-import Costos from "./Costos"
-import Error from "./Error"
-import Informes from "./Informes"
-import Login from "./Login"
-import MiCuenta from "./MiCuenta"
-import Productos from "./Productos"
-import Ventas from "./Ventas"
+import Costos from "./Costos";
+import Error from "./Error";
+import Informes from "./Informes";
+import Login from "./Login";
+import MiCuenta from "./MiCuenta";
+import Productos from "./Productos";
+import Ventas from "./Ventas";
+import Register from "./Register";
 
-export {Costos, Error, Informes, Login, MiCuenta, Productos, Ventas}
+export {
+  Costos,
+  Error,
+  Informes,
+  Login,
+  Register,
+  MiCuenta,
+  Productos,
+  Ventas,
+};
