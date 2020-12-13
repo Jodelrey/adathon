3e2d9ba162ff4a9257ed3d090e14381abe5b47f1
Josefina del Rey <delreyjo@gmail.com>
2020-12-13 08:14:32 -0300

    form

--- src/pages/Productos.js
+++ src/pages/Productos.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Container } from "../components/primitives";
+import Form from "../components/Form";
+import styled from "styled-components";
+
+const StyledContainer = styled(Container)`
+  width: 100%;
+  //min-height: calc(100vh - 120px);
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 const Productos = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <>
+      <StyledContainer>
+        <Form
+          heading={Productos}
+          product={true}
+          description={true}
+          cost={true}
+          delivery={true}
+        />
+      </StyledContainer>
+    </>
+  );
+};
 
-export default Productos
+export default Productos;
